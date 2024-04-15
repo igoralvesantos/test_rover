8805e8636d6a5aa1a416266a4a4111b3bac8d520
Igor Alves <igoralvesantos@gmail.com.br>
2024-04-15 07:07:15 -0300

    adds rover navigation service

---
+++ src/services/RoverNavigationService.ts
@@ -0,0 +1,86 @@
+import Plateau from '../entities/Plateau';
+import Position, { CardinalDirections } from '../entities/Position';
+import Rover from '../entities/Rover';
+
+export enum Commands {
+  Move = 'M',
+  RotateLeft = 'L',
+  RotateRight = 'R',
+}
+
+export enum RotationDirections {
+  Left = 'left',
+  Right = 'right',
+}
+
+class RoverNavigationService {
+  private _rover: Rover;
+  private _plateau: Plateau;
+  private static readonly cardinalDirectionMap = new Map([
+    [CardinalDirections.North, { left: CardinalDirections.West, right: CardinalDirections.East }],
+    [CardinalDirections.East, { left: CardinalDirections.North, right: CardinalDirections.South }],
+    [CardinalDirections.South, { left: CardinalDirections.East, right: CardinalDirections.West }],
+    [CardinalDirections.West, { left: CardinalDirections.South, right: CardinalDirections.North }],
+  ]);
+  private static readonly movementsMap = new Map([
+    [CardinalDirections.North, { x: 0, y: 1 }],
+    [CardinalDirections.East, { x: 1, y: 0 }],
+    [CardinalDirections.South, { x: 0, y: -1 }],
+    [CardinalDirections.West, { x: -1, y: 0 }],
+  ]);
+
+  constructor(rover: Rover, plateau: Plateau) {
+    this._rover = rover;
+    this._plateau = plateau;
+  }
+
+  executeCommand(command: Commands) {
+    switch (command) {
+      case Commands.Move:
+        this.move();
+        break;
+      case Commands.RotateLeft:
+        this.rotate(RotationDirections.Left);
+        break;
+      case Commands.RotateRight:
+        this.rotate(RotationDirections.Right);
+        break;
+    }
+  }
+
+  private move() {
+    const movement = RoverNavigationService.movementsMap.get(
+      this._rover.position.cardinalDirection,
+    );
+
+    if (!movement) {
+      throw new Error('Invalid direction: Rover cannot move in that direction.');
+    }
+
+    const newX = this._rover.position.x + movement.x;
+    const newY = this._rover.position.y + movement.y;
+
+    const newRoverPosition = new Position(newX, newY, this._rover.position.cardinalDirection);
+    const isRoverOffLimits = this._plateau.isOffLimits(newRoverPosition);
+
+    if (isRoverOffLimits) {
+      throw new Error('Invalid movement: Rover would leave the Plateau limits.');
+    }
+
+    this._rover.position = newRoverPosition;
+  }
+
+  private rotate(rotationDirection: RotationDirections) {
+    const currentCardinalDirection = RoverNavigationService.cardinalDirectionMap.get(
+      this._rover.position.cardinalDirection,
+    );
+
+    if (!currentCardinalDirection) {
+      throw new Error('Invalid direction: Rover cannot turn in that direction.');
+    }
+
+    this._rover.position.cardinalDirection = currentCardinalDirection[rotationDirection];
+  }
+}
+
+export default RoverNavigationService;
---
+++ tests/services/RoverNavigationService.test.ts
@@ -0,0 +1,42 @@
+import RoverNavigationService, { Commands } from '../../src/services/RoverNavigationService';
+import Plateau from '../../src/entities/Plateau';
+import Position, { CardinalDirections } from '../../src/entities/Position';
+import Rover from '../../src/entities/Rover';
+import Coordinates from '../../src/entities/Coordinates';
+
+describe('RoverNavigationService', () => {
+  const position = new Position(1, 2, CardinalDirections.North);
+  const rover = new Rover(position);
+  const minCoordinate = new Coordinates(0, 0);
+  const maxCoordinate = new Coordinates(5, 5);
+  const plateau = new Plateau(minCoordinate, maxCoordinate);
+  const roverNavigationService = new RoverNavigationService(rover, plateau);
+
+  it('should move rover', () => {
+    const newRoverPosition = new Position(1, 3, CardinalDirections.North);
+
+    roverNavigationService.executeCommand(Commands.Move);
+
+    expect(rover.position).toEqual(newRoverPosition);
+  });
+
+  it('should rotate rover left', () => {
+    roverNavigationService.executeCommand(Commands.RotateLeft);
+
+    expect(rover.position.cardinalDirection).toEqual(CardinalDirections.West);
+  });
+
+  it('should rotate rover right', () => {
+    roverNavigationService.executeCommand(Commands.RotateRight);
+
+    expect(rover.position.cardinalDirection).toEqual(CardinalDirections.North);
+  });
+
+  it('should throw error when rover moves off limits', () => {
+    rover.position = new Position(5, 5, CardinalDirections.North);
+
+    expect(() => roverNavigationService.executeCommand(Commands.Move)).toThrow(
+      'Invalid movement: Rover would leave the Plateau limits.',
+    );
+  });
+});
